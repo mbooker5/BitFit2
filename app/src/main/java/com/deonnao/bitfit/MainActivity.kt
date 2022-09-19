c84ae5e9869333466267575fe5321c24b324c521
deonnao <deonnaowens3@yahoo.com>
2022-09-19 17:05:35 -0500

    final commit

--- app/src/main/java/com/deonnao/bitfit/MainActivity.kt
+++ app/src/main/java/com/deonnao/bitfit/MainActivity.kt
@@ -27,10 +27,6 @@ import kotlinx.serialization.json.Json
 
 class MainActivity : AppCompatActivity() {
 
-    //var listOfItems = mutableListOf<Nutrition>()
-    //  lateinit var adapter: NutritionAdapter
-    //private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -38,90 +34,5 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         bottomNavigationView.setupWithNavController(navHostFragment.navController)
-
-
-        //binding = ActivityMainBinding.inflate(layoutInflater)
-        //val view = binding.root
-        //setContentView(view)
-
-        /* //values to reference the views from the layout file
-        val foodItem = findViewById<EditText>(R.id.etFoodItem)
-        val etCalories = findViewById<EditText>(R.id.etCalories)
-        val submitBtn = findViewById<Button>(R.id.submitBtn)
-
-        val onLongClickListener = object : NutritionAdapter.onLongClickListener {
-            override fun onItemLongClicked(position: Int) {
-                //Remove the item from the list
-                listOfItems.removeAt(position)
-                //Notify the adapter that our data set has changed
-                //adapter.notifyDataSetChanged()
-            }
-        }*/
-        /* // Lookup the recyclerview in activity layout
-        val recyclerView = findViewById<RecyclerView>(R.id.rvList)
-        // Create adapter passing in the listOfItems and the long click listener
-        adapter = NutritionAdapter(listOfItems, onLongClickListener)
-        // Attach the adapter to the recyclerview to populate items
-        recyclerView.adapter = adapter
-        // Set layout manager to position the items
-        recyclerView.layoutManager = LinearLayoutManager(this)
-
-        submitBtn.setOnClickListener {
-            //Grab the text that the user inputs
-            val item = foodItem.text.toString()
-            val calories = etCalories.text.toString()
-            getItems(item, calories)
-            foodItem.setText("")
-            etCalories.setText("")
-            foodItem.hideKeyboard()
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
-
-        lifecycleScope.launch {
-            (application as NutritionApplication).db.NutritionDao().getAll()
-                .collect { databaseList ->
-                    databaseList.map { entity ->
-                        Nutrition(
-                            entity.food.toString(),
-                            entity.calories.toString()
-                        )
-                    }.also { mappedList ->
-                        listOfItems.clear()
-                        listOfItems.addAll(mappedList)
-                        adapter.notifyDataSetChanged()
-                    }
-                }
-        }
-
-        /**
-         * This is the place with the error. I am trying to insert data into the database and
-         * not sure how to do it
-         */
-        /*let { list ->
-            lifecycleScope.launch(Dispatchers.IO) {
-                (application as NutritionApplication).db.NutritionDao().deleteAll()
-                (application as NutritionApplication).db.NutritionDao().insertAll(list.map {
-                    NutritionEntity(
-                        food = food,
-                        calories = calories
-                    )
-                })
-            }
-        }*/
-    }
-
-    fun getItems(item : String, calories : String) {
-        //Reset text field after user adds item to the wishlist
-        val wishItems = Nutrition(item, calories)
-        listOfItems.add(wishItems)
-        adapter.notifyItemInserted(listOfItems.size - 1)
-    }
-
-    //function to hide the keyboard after the user enters their desired item to the wishlist
-    fun View.hideKeyboard() {
-        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(windowToken, 0)
-    }*/
     }
 }
